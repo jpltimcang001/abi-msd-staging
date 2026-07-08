2c2917791d8ca3d42a3aee00da88ccd7a27dd641
Joseph Patrick Timcang <jpltimcang@1go.ph>
2026-07-08 09:08:06 +0800

    Pushing Latest updates

--- batch_schedule/resync-download.bat
+++ batch_schedule/resync-download.bat
@@ -1,9 +1,9 @@
 cd c:\
 cd %~dp0\..
 cls 
-REM php artisan api:allDownload --company="BII Live" --sales-office="750300" --so-short="UNS" --salesman="UNS-501"
-REM TIMEOUT /T 5
-REM php artisan api:allDownload --company="BII Live" --sales-office="750300" --so-short="UNS" --salesman="UNS-204"
-REM TIMEOUT /T 5
+php artisan api:allDownload --company="BII Live" --sales-office="750300" --so-short="UNS" --salesman="UNS-204"
+TIMEOUT /T 5
 php artisan api:allDownload --company="BII Live" --sales-office="750300" --so-short="UNS" --salesman="UNS-230"
+TIMEOUT /T 5
+php artisan api:allDownload --company="BII Live" --sales-office="750300" --so-short="UNS" --salesman="UNS-501"
 TIMEOUT /T 5
